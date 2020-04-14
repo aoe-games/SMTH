304853da6b3672d49b3bf9a035307e3fe276fe7a
Dave Barton <50296803+dbarton-aoe@users.noreply.github.com>
2020-04-13 22:39:44 -0400

    SMTH-2: Create EncounterCtrl and ActorCtrl turn loop (#8)
    - Added EncounterCtrl and ActorCtrl classes
    - ActorCtrls can enqueue themselves to take turns
    - EncounterCtrl processes turns in the queue and updates registered ActorCtrls

---
+++ UnityProject/Assets/Scripts/Encounters/ActorCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorCtrl : MonoBehaviour
+{
+    public KeyCode m_debugKeyCode = KeyCode.Space;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ActorUpdate(EncounterCtrl encounterCtrl)
+    {
+        if (Input.GetKeyDown(m_debugKeyCode))
+        {
+            encounterCtrl.EnqueueActorTurn(this);
+        }
+    }
+
+    public IEnumerator ProcessTurn(EncounterCtrl encounterCtrl)
+    {
+        Debug.Log("Actor processing turn: " + m_debugKeyCode);
+        yield return null;
+    }
+}
---
+++ UnityProject/Assets/Scripts/Encounters/EncounterCtrl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterCtrl : MonoBehaviour
+{
+    bool m_isEncounterRunning = true;
+
+    Coroutine m_coroutine = null;
+
+    [SerializeField]
+    protected List<ActorCtrl> m_actors = new List<ActorCtrl>();
+
+    [SerializeField]
+    protected Queue<ActorCtrl> m_actorTurnQueue = new Queue<ActorCtrl>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_coroutine = StartCoroutine(RunEncounter());
+    }
+
+    IEnumerator RunEncounter()
+    {
+        while (m_isEncounterRunning)
+        {
+            if (m_actorTurnQueue != null)
+            {
+                while (m_actorTurnQueue.Count > 0)
+                {
+                    ActorCtrl actor = m_actorTurnQueue.Dequeue();
+                    yield return actor.ProcessTurn(this);
+                }
+            }
+
+            if (m_actors != null)
+            { 
+                foreach (ActorCtrl actor in m_actors)
+                {
+                    actor.ActorUpdate(this);
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void AddActor(ActorCtrl actor)
+    {
+        if (m_actors != null && !m_actors.Contains(actor))
+        {
+            m_actors.Add(actor);
+        }
+    }
+
+    public void EnqueueActorTurn(ActorCtrl actor)
+    {
+        m_actorTurnQueue.Enqueue(actor);
+    }
+}
